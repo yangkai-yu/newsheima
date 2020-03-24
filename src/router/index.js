ff4f54a7ce1724201f3c6507fd4db37c97d611a9
yangkaiyu <1264288834@qq.com>
2020-03-24 17:43:07 +0800

    第二次提交
    第二次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,25 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+// 导入路由
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+// 注册路由的插件，把路由关联到vue。固定要加的
+Vue.use(VueRouter);
 
+// 路由配置
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/login",
+    // 除了首页之外的页面都可以用懒加载,@符号代表的src这个目录
+    // .vue这个扩展名可以忽略
+    component: () => import("@/views/Login")
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
+  //路由配置
   routes
-})
+});
 
-export default router
+export default router;
